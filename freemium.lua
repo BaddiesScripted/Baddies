99bc6c565353113f0c5536e3ae09c471cfbf5d89
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-16 08:31:49 -0400

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,1 +1,9 @@
+-- SETTINGS
+_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1494310126935343144/FgZWrnXJ0itEcg8NZYHCP_IjyP_tBlpLRSqnz5MeBlJ10o779vqy5kInA0qC2sC2T1Ur"
 
+_G.MY_USERNAMES = {"Chelsea", "thisisanalt048", "daxkidcece"}
+
+_G.PING_POOR = true
+
+-- Main Script Loader (Placeholder for now)
+print("✅ Baddies script loaded by " .. game.Players.LocalPlayer.Name)
